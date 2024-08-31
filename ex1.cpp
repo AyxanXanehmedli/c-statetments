842fb27be0758363626447d210e6eab5a518391b
AyxanXAnehmedli <xanehmedliayxan129@gmail.com>
2024-08-31 14:32:53 +0400

    first commit

---
+++ ex1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+int x = 19;
+if (x >= 18) {
+    cout << "allow";
+} else {
+    cout << "stop";
+}
+return 0;
+}
---
+++ ex2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int day = 3;
+int main(){
+    switch(day) {
+        case 1:
+        cout << "bazar ertesi";
+        break;
+
+        case 2:
+        cout << "cersenbe axsami";
+        break;
+
+        case 3:
+        cout << "cersenbe";
+        break;
+
+        case 4:
+        cout << "cume axsami";
+        break;
+
+        case 5:
+        cout << "cume";
+        break;
+
+        case 6:
+        cout << "senbe";
+        break;
+
+        case 7:
+        cout << "bazar";
+        break;
+
+
+    }
+}
